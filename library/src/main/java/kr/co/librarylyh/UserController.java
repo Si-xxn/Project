323c7e5ef65df5ea50d449e08063b0709291bfad
10-4\mit <tldms7208@naver.com>
2024-09-08 09:41:41 +0900

    v1.0.4

--- library/src/main/java/kr/co/librarylyh/UserController.java
+++ library/src/main/java/kr/co/librarylyh/UserController.java
@@ -1,8 +1,15 @@
 package kr.co.librarylyh;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,6 +17,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import kr.co.librarylyh.domain.UserVO;
 import kr.co.librarylyh.service.UserService;
+import kr.co.librarylyh.utils.SessionConst;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
@@ -22,32 +30,86 @@ public class UserController {
 		private UserService service;
 		
 		@GetMapping({"/home"})
-		public void mainPage() {
+		public String home() {
 			
+			String homePage = "/library/home";
+			
+			return homePage;
 		}
 		
-		@GetMapping({"/login", "/register"})
-		public void userPage() {
+		@GetMapping("/login")
+		public String loginForm() {
+			String loginPage = "/library/login";
 			
+			return loginPage;		
 		}
 		
-		@PostMapping("/login")
-		public String login(UserVO user, RedirectAttributes rttr) {
-			log.info("login : " + user);
+		/*
+		 * @PostMapping("/loginConfirm") public String loginConfirm(UserVO user,
+		 * HttpSession session) { String nextPage = "/library/loginCheck";
+		 * 
+		 * user = service.login(user); if(user == null) { nextPage = "/library/user"; }
+		 * else { session.setAttribute("loginUser", user);
+		 * session.setMaxInactiveInterval(60 * 30); }
+		 * 
+		 * return nextPage; }
+		 * 
+		 * @GetMapping("/logoutForm") public String logoutForm(@CookieValue(value =
+		 * "userLogin", required = false) String loginUser, HttpServletResponse
+		 * response) { String nextPage = "redirect:/member/";
+		 * 
+		 * Cookie cookie = new Cookie("loginUser", loginUser); cookie.setMaxAge(0);
+		 * 
+		 * response.addCookie(cookie);
+		 * 
+		 * return nextPage; }
+		 * 
+		 * @GetMapping("/findPassword") public String findPassword() { String nextPage =
+		 * "/library/findPassword"; return nextPage; }
+		 * 
+		 * @PostMapping("/findPassword") public String findPassword(UserVO user,
+		 * HttpSession session) { String nextPage = "library/findPassword";
+		 * 
+		 * if(user == null) { nextPage = "library/login"; } else { nextPage =
+		 * "/library/newPassword"; } return nextPage; }
+		 */
+		
+		@GetMapping("/")
+		public String homeLogin(HttpServletRequest request, Model model) {
+			// 세션이 없으면 home
+			HttpSession session = request.getSession(false);
+			if(session == null) {
+				return "home";
+			}
 			
-			service.login(user);
-			
-			rttr.addAttribute("result", user.getName());
-			
-			return "redirect:/users/home";
+			UserVO user = (UserVO)session.getAttribute(SessionConst.LOGIN_USER);
+			// 세션에 회원 데이터가 없으면 home
+			if(user == null) {
+				return "home";
+			}
+			model.addAttribute("loginUser", user);
+			return "login";
 		}
 		
-		@PostMapping("/register")
-		public String register(UserVO user) {
+		// 세션이 유지되면 로그인으로 이동
+		@PostMapping("/createAccountForm")
+		public void createAccountForm() {
+		
+		}
+		
+		@PostMapping("createAccountConfirm")
+		public String createAccountConfirm(UserVO user) {
+			String nextPage = "/library/login";
+			
 			service.register(user);
-			log.info("회원정보 : " + user.getU_id());
-										//WJ5SRX6YML
-			return "redirect:/users/login";
+			
+			if(user == null) {
+				nextPage = "/library/login";
+			} else {
+				nextPage = "library/mypage";
+			}
+			
+			return nextPage;
 		}
 		
 		@GetMapping({"/get", "/modify"}) 
@@ -56,6 +118,22 @@ public class UserController {
 			log.info("UserController.get | modify 메서드 실행");
 			model.addAttribute("board", service.get(u_id));
 			// 서비스 계층에 get메서드에 u_id 값을 넣어 주면 SQL 처리후 객체 나옴
+			
+		}
+		
+		@GetMapping("/modify")
+		public String modifyForm(UserVO user, HttpSession session) {
+			String nextPage = "library/mypage";
+			
+			user = (UserVO) session.getAttribute("loginUser");
+			
+			if(user == null) {
+				nextPage = "library/home";
+			} else {
+				nextPage = "library/modify";
+			}
+			
+			return nextPage;	
 		}
 		
 		@PostMapping("/modify") 
--- library/src/main/java/kr/co/librarylyh/service/UserServiceImpl.java
+++ library/src/main/java/kr/co/librarylyh/service/UserServiceImpl.java
@@ -13,16 +13,34 @@ import lombok.extern.log4j.Log4j2;
 @Service // root-context 항목 추가
 @AllArgsConstructor
 public class UserServiceImpl implements UserService {
-
+	
+	final static public int USER_ACCOUNT_ALREADY_EXIST = 0;
+	final static public int USER_ACCOUNT_CREATE_SUCCESS = 1;
+	final static public int USER_ACCOUNT_CREATE_FAIL = -1;
+	
 	@Setter(onMethod_ = @Autowired)
 	private UserMapper mapper;
 	
 	@Override
-	public void register(UserVO user) {
+	public int register(UserVO user) {
 		
-		log.info("UserServiceImpl.join 메서드 실행");
+		log.info("UserServiceImpl.register 메서드 실행");
 		
-		mapper.insertSelectUid(user);
+		boolean find_id = mapper.findId(user.getId());
+		
+		if(!find_id) {
+			int result = mapper.insertSelectUid(user);
+			
+			if(result > 0) {
+				return USER_ACCOUNT_CREATE_SUCCESS;
+			} else {
+				return USER_ACCOUNT_CREATE_FAIL;
+			} 
+			
+		} else {
+			return USER_ACCOUNT_ALREADY_EXIST;
+		}
+		
 	}
 
 	@Override
@@ -53,6 +71,27 @@ public class UserServiceImpl implements UserService {
 	public UserVO login(UserVO user) {
 		log.info("UserServiceImpl.login 메서드 실행");
 		return user = mapper.login(user);
+	}
+
+	@Override
+	public boolean findId(String id) {
+		
+		boolean userId = mapper.findId(id);
+		
+		if(userId == false) {
+			return false;	
+		}
+		return true;		
+		
+	}
+
+	@Override
+	public int findPassword(UserVO user) {
+		
+		user.setId("test333");
+		user.setPhone("010-1234-9876");
+		
+		return 0;
 	}
 
 
